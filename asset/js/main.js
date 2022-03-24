ee7476721ea4c4f01b7e798bad8f9fec4d428efa
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-24 15:36:40 +0100

    creaz file e stesura array team

---
+++ asset/js/main.js
@@ -0,0 +1,43 @@
+// Per la pagina 'Chi siamo' del nostro sito aziendale, dobbiamo sviluppare la sezione "Il nostro team".
+// Definiamo un array di oggetti che rappresentano i membri del team. 
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Milestone 1: stampare in console l'elenco dei membri del team, 
+// scrivendo separatamente i dettagli di ciascun componente.
+// Milestone 2: stampare i dati all'interno di un contenitore nella pagina html in modo dinamico, 
+// creando per ciascun membro del team un elemento che conterrà i suoi dati.
+// BONUS: stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.
+
+// creazione array di oggetti
+let team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'img/walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'img/scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'img/barbara-ramos-graphic-designer.jpg',
+    }
+];
+
